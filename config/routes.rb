6af0520825ca43d6fc3a4410eea170878110531d
AkermanGreg <gregoryakerman@gmail.com>
2014-11-18 14:45:23 -0800

    Update for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'home#index'.
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,3 +56,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
